cf9cba5f6486116253b88c31163f319945461aea
Dudemario <vincentjsun@gmail.com>
2023-08-19 17:09:24 -0400

    Update App.js

--- karaoke_webapp/src/App.js
+++ karaoke_webapp/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import './pitch.js'
+import Graph from './components/Graph';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={
+        {
+          width: '600px',
+          height: '300px'
+        }
+      }>
+      <Graph message={[[3,9,6],[4,2,6]]}/>
+      </div>
+      <Router>
+        <Routes>
+          <Route path = "/login" element={<Login />} />
+          <Route path = "/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
