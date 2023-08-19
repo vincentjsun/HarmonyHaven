06601c466060eddc4b88e8fb3416021d13e08238
Dudemario <vincentjsun@gmail.com>
2023-08-19 19:28:57 -0400

    fix karaoke

--- karaoke_webapp/src/App.js
+++ karaoke_webapp/src/App.js
@@ -3,16 +3,15 @@ import React, { useState } from 'react';
 import Pitch from './Pitch';
 import ResultScreen from './ResultScreen';
 import { callMeMaybe, perc } from './SongPitches'; 
-import FilePitch from './FilePitch';
 import Graph from './components/Graph';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
+let stop = false;
 function App() {
   const [shouldUpdate, setShouldUpdate] = useState(1);
   const [detectedNotes, setDetectedNotes] = useState([]);
-  const [stop, setStop] = useState(false);
+  //const [stop, setStop] = useState(false);
   var previousValueToDisplay = 0;
   var smoothingCount = 0;
   var smoothingThreshold = 20;
@@ -43,6 +42,7 @@ function App() {
 
   const updateDetectedNotes = newDetectedNote => {
     if (shouldUpdate === 1 && !stop) {
+      console.log(stop);
       setDetectedNotes(prevNotes => [...prevNotes, newDetectedNote]);
     }
   };
@@ -53,20 +53,20 @@ function App() {
 
   const visualize = (audioContext, analyser) => {
     if (shouldUpdate === 1) {
-      const dataArray = new Uint8Array(analyser.frequencyBinCount);
-      analyser.getByteFrequencyData(dataArray);
+      // const dataArray = new Uint8Array(analyser.frequencyBinCount);
+      // analyser.getByteFrequencyData(dataArray);
 
-      const maxFrequencyIndex = dataArray.indexOf(Math.max(...dataArray));
-      const maxFrequency = maxFrequencyIndex * audioContext.sampleRate / analyser.fftSize;
+      // const maxFrequencyIndex = dataArray.indexOf(Math.max(...dataArray));
+      // const maxFrequency = maxFrequencyIndex * audioContext.sampleRate / analyser.fftSize;
 
-      // var bufferLength = analyser.fftSize;
-      // var buffer = new Float32Array(bufferLength);
-      // analyser.getFloatTimeDomainData(buffer);
-      // var autoCorrelateValue = autoCorrelate(buffer, audioContext.sampleRate)
+      var bufferLength = analyser.fftSize;
+      var buffer = new Float32Array(bufferLength);
+      analyser.getFloatTimeDomainData(buffer);
+      var closestNote = autoCorrelate(buffer, audioContext.sampleRate)
 
       // const noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
       // const closestNote = noteStrings[frequencyToNote(maxFrequency) % 12] || 'no sound';
-      const closestNote = maxFrequency || -1;
+      //const closestNote = maxFrequency || -1;
       // const closestNote = noteStrings[frequencyToNote(autoCorrelateValue) % 12] || 'no sound';
       if (noteIsSimilarEnough(closestNote)) {
         if (smoothingCount < smoothingCountThreshold) {
@@ -79,6 +79,7 @@ function App() {
         previousValueToDisplay = closestNote;
         smoothingCount = 0;
       }
+      //console.log(detectedNotes);
       updateDetectedNotes(closestNote);
 
       setTimeout(() => {
@@ -87,55 +88,57 @@ function App() {
     }
   };
 
-  // function autoCorrelate( buf, sampleRate ) {
-  //   // Implements the ACF2+ algorithm
-  //   var SIZE = buf.length;
-  //   var rms = 0;
+  function autoCorrelate( buf, sampleRate ) {
+    // Implements the ACF2+ algorithm
+    var SIZE = buf.length;
+    var rms = 0;
   
-  //   for (var i=0;i<SIZE;i++) {
-  //     var val = buf[i];
-  //     rms += val*val;
-  //   }
-  //   rms = Math.sqrt(rms/SIZE);
-  //   if (rms<0.01) // not enough signal
-  //     return -1;
+    for (var i=0;i<SIZE;i++) {
+      var val = buf[i];
+      rms += val*val;
+    }
+    rms = Math.sqrt(rms/SIZE);
+    if (rms<0.01) // not enough signal
+      return -1;
   
-  //   var r1=0, r2=SIZE-1, thres=0.2;
-  //   for (i=0; i<SIZE/2; i++) 
-  //     if (Math.abs(buf[i])<thres) { r1=i; break; }
-  //   for (i=1; i<SIZE/2; i++)
-  //     if (Math.abs(buf[SIZE-i])<thres) { r2=SIZE-i; break; }
+    var r1=0, r2=SIZE-1, thres=0.2;
+    for (i=0; i<SIZE/2; i++) 
+      if (Math.abs(buf[i])<thres) { r1=i; break; }
+    for (i=1; i<SIZE/2; i++)
+      if (Math.abs(buf[SIZE-i])<thres) { r2=SIZE-i; break; }
   
-  //   buf = buf.slice(r1,r2);
-  //   SIZE = buf.length;
+    buf = buf.slice(r1,r2);
+    SIZE = buf.length;
   
-  //   var c = new Array(SIZE).fill(0);
-  //   for (i=0; i<SIZE; i++)
-  //     for (var j=0; j<SIZE-i; j++)
-  //       c[i] = c[i] + buf[j]*buf[j+i];
+    var c = new Array(SIZE).fill(0);
+    for (i=0; i<SIZE; i++)
+      for (var j=0; j<SIZE-i; j++)
+        c[i] = c[i] + buf[j]*buf[j+i];
   
-  //   var d=0; while (c[d]>c[d+1]) d++;
-  //   var maxval=-1, maxpos=-1;
-  //   for (i=d; i<SIZE; i++) {
-  //     if (c[i] > maxval) {
-  //       maxval = c[i];
-  //       maxpos = i;
-  //     }
-  //   }
-  //   var T0 = maxpos;
+    var d=0; while (c[d]>c[d+1]) d++;
+    var maxval=-1, maxpos=-1;
+    for (i=d; i<SIZE; i++) {
+      if (c[i] > maxval) {
+        maxval = c[i];
+        maxpos = i;
+      }
+    }
+    var T0 = maxpos;
   
-  //   var x1=c[T0-1], x2=c[T0], x3=c[T0+1];
-  //   var a = (x1 + x3 - 2*x2)/2;
-  //   var b = (x3 - x1)/2;
-  //   if (a) T0 = T0 - b/(2*a);
+    var x1=c[T0-1], x2=c[T0], x3=c[T0+1];
+    var a = (x1 + x3 - 2*x2)/2;
+    var b = (x3 - x1)/2;
+    if (a) T0 = T0 - b/(2*a);
   
-  //   return sampleRate/T0;
-  // }
+    return sampleRate/T0;
+  }
   
 
   const stopUpdate = () => {
+    console.log("hi");
     setShouldUpdate(0);
-    setStop(true);
+    stop = true;
+    //setStop(true);
     console.log(detectedNotes);
   };
 
@@ -152,27 +155,26 @@ function App() {
 
         <canvas id="visualizer" width="400" height="200"></canvas>
 
-        {shouldUpdate === 1 && (
+        {/* {{shouldUpdate === 1 && (
           <ul id="detectedNotes">
             {detectedNotes.map((note, index) => (
               <li key={index}>{note}</li>
             ))}
           </ul>
-        )}
+        )} */}
 
         {shouldUpdate === 0 && (
           <ResultScreen user={detectedNotes} orig={callMeMaybe} perc={perc}/>
         )}
       </header>
       <Pitch visualize={visualize} />
-      <FilePitch/>
       <div style={
         {
           width: '600px',
           height: '300px'
         }
       }>
-      <Graph message={[[3,9,6],[4,2,6]]}/>
+      <Graph message={[callMeMaybe,detectedNotes]}/>
       </div>
       <Router>
         <Routes>
