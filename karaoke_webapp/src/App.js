ce21a139a8c55c74e9dcde2709e8f0dfcd86d3b6
Tvomato <JustinLi2013@gmail.com>
2023-08-20 01:49:42 -0400

    merged

--- karaoke_webapp/src/App.js
+++ karaoke_webapp/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './pages/App.css';
 import React, { useState } from 'react';
 import { callMeMaybe, perc } from './components/SongPitches'; 
 import Graph from './components/Graph';
--- karaoke_webapp/src/index.js
+++ karaoke_webapp/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './pages/App';
+import App from './App';
 import Test from './pages/Search';
 import Test2 from './pages/Song';
 import Router from './pages/Router';
