6fef766fdd3e937c77f520597afef25a0789d94d
Dudemario <vincentjsun@gmail.com>
2023-08-20 09:32:12 -0400

    Update Home.js

--- karaoke_webapp/src/pages/Home.js
+++ karaoke_webapp/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import Pitch from '../components/Pitch';
 import screenshot from '../assets/screenshot.png';
 import { useLocation, useNavigate } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function Home() {
     <div className='apphome'>
       <h1 className='tagline'>website tagline</h1>
       <h3 className='text'>Get started now or learn more:</h3>
+      <Pitch />
       <button className='navbutton' onClick={checkout}>Try Karaoke</button>
       <h3 className='text3'>Example:</h3>
       <img className='screen' src={screenshot}/>
